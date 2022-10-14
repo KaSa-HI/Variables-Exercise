9bd10ab808149477c0e42c58f1a1c3b6f4512377
koikiri <kahvisabatino@gmail.com>
2022-10-14 10:21:59 -1000

    All Solutions

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Variables Exercise");
+
+            string dogName = "Arai";
+            int dogAge = 8;
+            char myDog = '4';
+            bool myDogIsFawn = true;
+            bool isHappy = false;
+            double pugHeight = 24.3;
+            decimal pugWeight = 15.8M;
+
+            Console.WriteLine(@$"My Pugs name is {dogName}, He is {dogAge} years old, He is healthy with {myDog} legs. Is he fawn coloured? {myDogIsFawn}");
+            Console.WriteLine(@$"Arais height is {pugHeight}inches. This ball of loverolls is {pugWeight}lbs.");
         }
     }
 }
